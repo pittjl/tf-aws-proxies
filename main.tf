cb8bf916e8962289869c20afdf25c6578d316135
John Pittman <throne@gmail.com>
2022-08-24 16:34:06 -0400

    more stuff

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ module "ec2" {
   source     = "./modules/ec2"
   namespace  = var.namespace
   ip_address = var.ip_address
+  instance_count = var.instance_count
   vpc        = module.networking.vpc
   sg_pub_id  = module.networking.sg_pub_id
   key_name   = module.ssh-key.key_name
